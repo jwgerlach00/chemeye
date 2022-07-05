4019d75579513ccc420fd6276f02b7659821284d
jacob_personal <jwgerlach00@gmail.com>
2022-07-05 14:49:04 -0400

    setup 'naclo' --> 'chemeye'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chemeye',
-    version='0.0.01',
+    version='0.0.2',
     license='MIT',
     author='Jacob Gerlach',
     author_email='jwgerlach00@gmail.com',
@@ -11,7 +11,7 @@ setup(
     description='Visualization toolset for small molecule drug discovery',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    package_data={'naclo': ['assets/default_simmat_options.json']},
+    package_data={'chemeye': ['assets/default_simmat_options.json']},
     python_requires='>=3.6',
     install_requires=[
         'naclo',
