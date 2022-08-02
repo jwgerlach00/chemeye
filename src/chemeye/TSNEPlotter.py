5182e23bd23979365e5ed664b2eef1b4fedbc1d8
jacob_personal <jwgerlach00@gmail.com>
2022-08-02 11:34:36 -0400

    Added NaN -> String 'missing color' replacement because px doesn't accept NaN as color

--- src/chemeye/TSNEPlotter.py
+++ src/chemeye/TSNEPlotter.py
@@ -2,6 +2,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from typing import Iterable, Optional
+import pandas as pd
 
 from chemeye.arrays import tsne
 
@@ -12,7 +13,7 @@ class TSNEPlotter:
         
     def plot(self, x_name, y_name, color_category:Optional[Iterable]=None) -> go.Figure:
         arr = tsne(self.__descriptors)
-        df = ({
+        df = pd.DataFrame({
             x_name: arr[:, 0],
             y_name: arr[:, 1]
         })
@@ -21,7 +22,9 @@ class TSNEPlotter:
         color = None
         if color_category:
             df['color'] = color_category
-            opacity = 0.25
+            opacity = 0.5
             color = 'color'
+
+        df['color'] = df['color'].fillna('missing color')  # Replace NaN w/ string bc px doesn't like NaN
             
         return px.scatter(df, x=x_name, y=y_name, color=color, render_mode='svg', opacity=opacity)
